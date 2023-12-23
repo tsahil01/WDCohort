4275cfdc092a4abd75657172c7403c6f779fcda8
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-23 21:04:30 +0530

    Promises-1 in JS

--- week-1/1.3_Aync_await_callback_promises.js
+++ week-1/1.3_Aync_await_callback_promises.js
@@ -63,11 +63,63 @@ function printCounter(){
 // Async => http://latentflip.com/loupe/?code=CmZ1bmN0aW9uIHByaW50KCl7CiAgICBjb25zb2xlLmxvZygiaW5zaWRlIHByaW50Iik7Cn0KCnNldFRpbWVvdXQocHJpbnQsIDEwMDApCgpjb25zb2xlLmxvZygiaGVsbG8iKTsKY29uc29sZS5sb2coIkhlZWxsb28iKTsKCmZ1bmN0aW9uIGEoKXsKICAgIGIoKQogICAgY29uc29sZS5sb2coIkhJIikKfQoKCmZ1bmN0aW9uIGEoKXsKICAgIGNvbnNvbGUubG9nKCJCeWUiKQp9CgoKYSgpCg%3D%3D!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
 
 // Here callStack is simple stack which puts the tasks.
-// Web Api keeps the async task till it complets
+// Web API keeps the async task till it complets. 
+// Web API runs on different thread
 // Callback Queue keeps the completed async tasks and the puts in the callback stack.
 
 // Chat Gpt response =>
-// When you call a function, it goes on the plate stack (call stack).
+// When you call a funct    ion, it goes on the plate stack (call stack).
 // If a task takes time (like waiting for a timer), it goes to the helper (Web API).
 // When the helper finishes, it writes a note and puts it in the line (callback queue).
 // The event loop is like a person checking if you're done with your plates. If you are, they take the notes from the line and start working on those tasks.
+
+
+
+// Here is the COMPLEX TOPIC =>
+// PROMISES =>
+
+// Callback hell?? ->
+// Async call chaining =>
+
+// setTimeout(function(){
+//     console.log("Hello 1");
+//     setTimeout(function(){
+//         console.log("Hello 2");
+//     }, 2000)
+// }, 1000)
+
+// This above is an example of Async calling channing.. Which is kind of tricky or difficult. Hence promises were introduced.
+// to avoid callbacks Promises were introduced!
+
+
+
+// Code with callback =>
+// setTimeout( ()=> {
+//     console.log("medicine 1 recvd");
+// }, 2000)
+
+
+// Same Code with Promises =>
+function promiseBody(resolve){
+    setTimeout(resolve, 2000)
+}
+function medicine1Get(){
+    return new Promise(promiseBody)
+}
+
+
+let medicine = medicine1Get()
+
+console.log(medicine); // OP=> Promise { <pending> }
+
+medicine.then( function(){
+    console.log("medicine 1 recvd");
+})
+
+// In promises Whenever I called a callback func it immediately gives me a promise  
+
+// Impt =>
+// fs.read('a.txt', callBackFunc ) // simple callback
+
+// Another way to use promises using just .then() =>
+// fs.read('a.txt').then( callBackFunc )
