722aee3e456f3eae8238eb479f8ecf355edb78f5
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2024-01-06 17:38:26 +0530

    JSON Web Tokens

---
+++ week-3/3.2.1_jws.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const jwtPassword = "123456";
+
+const app = express();
+app.use(express.json())
+
+const ALL_USERS = [
+  {
+    username: "harkirat@gmail.com",
+    password: "123",
+    name: "harkirat singh",
+  },
+  {
+    username: "raman@gmail.com",
+    password: "123321",
+    name: "Raman singh",
+  },
+  {
+    username: "priya@gmail.com",
+    password: "123321",
+    name: "Priya kumari",
+  },
+];
+
+
+function userExists(username, password) {
+  // write logic to return true or false if this user exists
+  // in ALL_USERS array
+  let userFound = false;
+
+  ALL_USERS.forEach((data) => {
+    if (data.username === username && data.password === password) {
+      userFound = true;
+    }
+  });
+
+  return userFound;
+}
+
+
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: "User doesnt exist in our in memory db",
+    });
+  }
+
+  var token = jwt.sign({ username: username }, "shhhhh"); // to create token
+  return res.json({
+    token,
+  });
+});
+
+app.get("/users", function (req, res) {
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, jwtPassword); // original JSON having data              
+    const username = decoded.username;
+    res.json({decoded})
+    // return a list of users other than this username
+  } catch (err) {
+    return res.status(403).json({
+      msg: "Invalid token",
+    });
+  }
+});
+
+app.listen(3000)
