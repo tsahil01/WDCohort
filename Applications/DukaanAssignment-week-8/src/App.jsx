50b847d222bdead265845eb270186a9ac866c80d
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2024-01-21 17:06:25 +0530

    Add sidebar to App component

--- Applications/DukaanAssignment-week-8/src/App.jsx
+++ Applications/DukaanAssignment-week-8/src/App.jsx
@@ -1,11 +1,19 @@
 import Header from "./component/Header"
+import Sidebar from "./component/Sidebar/Sidebar"
 import MainSection from "./component/main/MainSection"
 
 function App() {
   return (
     <>
-    <Header title={"Payouts"}/>
-    <MainSection/>
+    <div className="flex">
+      <div className="hidden md:block w-64  h-dvh bg-black">
+      <Sidebar/>
+      </div>
+      <div className="w-full">  
+        <Header title={"Payouts"}/>
+        <MainSection/>
+      </div>
+    </div>
     </>
   )
 }
---
+++ Applications/DukaanAssignment-week-8/src/component/Sidebar/Sidebar.jsx
@@ -0,0 +1,5 @@
+export default function Sidebar(){
+    return<>
+        <div className="text-lg">Hello</div>
+    </>
+}
