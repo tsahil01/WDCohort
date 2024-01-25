4d21b7671b88356d0414a5cafa0a591bb454ea5d
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2024-01-25 17:10:51 +0530

    7.4 code

---
+++ week-7/7.4/7.4/7.4-async-queries/src/atoms.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { atom, selector } from "recoil";
+
+export const notifications = atom({
+    key: "networkAtom",
+    default: selector({
+        key: "networkAtomSelector",
+        get: async () => {
+            const res = await axios.get('https://sum-server.100xdevs.com/notifications')
+            return res.data
+            
+        }
+    })
+});
+
+export const totalNotificationSelector = selector({
+    key: "totalNotificationSelector",
+    get: ({get}) => {
+        const allNotifications = get(notifications);
+        return allNotifications.network + 
+        allNotifications.jobs + 
+        allNotifications.notifications + 
+        allNotifications.messaging
+    }
+})
