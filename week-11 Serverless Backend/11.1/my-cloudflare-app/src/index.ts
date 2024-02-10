cbbf3e75112272dfd06b11202a41d96538676eb7
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-10 20:27:23 +0530

    Add express dependency and update fetch function

--- week-11 Serverless Backend/11.1/my-cloudflare-app/src/index.ts
+++ week-11 Serverless Backend/11.1/my-cloudflare-app/src/index.ts
@@ -27,6 +27,18 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		console.log(request.body);
+		console.log(request.headers);
+		console.log(request.method);
+		
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
 	},
 };
