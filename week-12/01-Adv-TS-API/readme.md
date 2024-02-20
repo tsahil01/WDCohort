39a9c351dff86324adf40da317fea267c0441acd
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-20 22:55:21 +0530

    Type infer in zod

--- week-12/01-Adv-TS-API/readme.md
+++ week-12/01-Adv-TS-API/readme.md
@@ -167,5 +167,39 @@ const handleEvent = (event: ExcludeEvent) => {
 };
 
 handleEvent('click'); // OK
+```
 
+## Type interface in ZOD =>
+![alt text](image.png)
+
+```ts
+import { z } from 'zod';
+import express from "express";
+
+const app = express();
+
+// Define the schema for profile update
+const userProfileSchema = z.object({
+  name: z.string().min(1, { message: "Name cannot be empty" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  age: z.number().min(18, { message: "You must be at least 18 years old" }).optional(),
+});
+
+type UserProfile = z.infer<typeof userProfileSchema>;
+
+app.put("/user", (req, res) => {
+  const { success } = userProfileSchema.safeParse(req.body);
+  const updateBody: UserProfile = req.body; // how to assign a type to updateBody?
+
+  if (!success) {
+    res.status(411).json({});
+    return
+  }
+  // update database here
+  res.json({
+    message: "User updated"
+  })
+});
+
+app.listen(3000);
 ```
--- week-12/01-Adv-TS-API/src/1_example.ts
+++ week-12/01-Adv-TS-API/src/1_example.ts
@@ -1,9 +1,29 @@
-type EventType = 'click' | 'scroll' | 'mousemove';
-type ExcludeEvent = Exclude<EventType, 'scroll'>; // 'click' | 'mousemove'
+import { z } from 'zod';
+import express from "express";
 
-const handleEvent = (event: ExcludeEvent) => {
-  console.log(`Handling event: ${event}`);
-};
+const app = express();
 
-handleEvent('click'); // OK
+// Define the schema for profile update
+const userProfileSchema = z.object({
+  name: z.string().min(1, { message: "Name cannot be empty" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  age: z.number().min(18, { message: "You must be at least 18 years old" }).optional(),
+});
 
+type UserProfile = z.infer<typeof userProfileSchema>;
+
+app.put("/user", (req, res) => {
+  const { success } = userProfileSchema.safeParse(req.body);
+  const updateBody: UserProfile = req.body; // how to assign a type to updateBody?
+
+  if (!success) {
+    res.status(411).json({});
+    return
+  }
+  // update database here
+  res.json({
+    message: "User updated"
+  })
+});
+
+app.listen(3000);
