2f603841df150a2c0d728b9c65a9996490ebacc9
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-03-02 20:07:44 +0530

    layouts

--- week-14/01/Next/app-1/app/layout.tsx
+++ week-14/01/Next/app-1/app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        hi there
+        {children}
+        </body>
     </html>
   );
 }
