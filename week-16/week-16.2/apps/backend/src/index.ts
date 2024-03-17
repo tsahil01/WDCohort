6fce4af53cd6500530a104946d5798bbee1600c0
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-03-17 20:22:00 +0530

    backend

--- week-16/week-16.2/apps/backend/src/index.ts
+++ week-16/week-16.2/apps/backend/src/index.ts
@@ -2,3 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req:Request, res:any)=>{
+    res.json({
+        msg: "Hello"
+    })
+})
+
+
+app.listen(4000, ()=>console.log("Running Backend on 4000"));
