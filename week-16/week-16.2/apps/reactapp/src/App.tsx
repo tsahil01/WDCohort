c0117647064fc08d9e7cbee4aa19c8cfdf627c24
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-03-17 19:52:45 +0530

    backend init

--- week-16/week-16.2/apps/reactapp/src/App.tsx
+++ week-16/week-16.2/apps/reactapp/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@repo/ui/button";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,8 @@ function App() {
   return (
     <>
       <div>
+
+      <Button appName="react app">Hello</Button>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
