6a172249b96a09af38dadc5262fe15b01be6b179
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-03-24 22:18:42 +0530

    next auth route.ts

---
+++ week-16 offline/2-NextAuth/my-app/src/app/api/auth/[...nextAuth]/route.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+        name: 'Credentials',
+        credentials: {
+          username: { label: 'email', type: 'text', placeholder: '' },
+          password: { label: 'password', type: 'password', placeholder: '' },
+        },
+        async authorize(credentials: any) {
+            
+            return {
+                id: "user1"
+            };
+        },
+      })
+  ],
+})
+
+export const GET = handler;
